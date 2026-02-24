0abbe1fde014d0cf57eb17027a0ed31616a81f15
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-02-24 19:07:21 +0000

    refactored the Player portion in the main and is now a Player Constructor

--- src/main.cpp
+++ src/main.cpp
@@ -100,6 +100,12 @@ class ScoreDisplay {
 
 class Player {
     public:
+        Player(int starting_x, int starting_y, bn::fixed player_speed, bn::size player_size) : 
+            sprite(bn::sprite_items::dot.create_sprite(starting_x, starting_y)),
+            speed(player_speed),
+            size(player_size),
+            bounding_box(create_bounding_box(sprite, size))
+        {}
         /**
          * Update the position and bounding box of the player based on d-pad movement.
          */
@@ -130,12 +136,7 @@ int main() {
 
     // Create a player and initialize it
     // TODO: we will move the initialization logic to a constructor.
-    Player player = Player();
-    player.sprite.set_x(44);
-    player.sprite.set_y(22);
-    player.speed = 1.5;
-    player.size = PLAYER_SIZE;
-    player.bounding_box = create_bounding_box(player.sprite, player.size);
+    Player player = Player(22, 44, 3.5, PLAYER_SIZE);
 
     bn::sprite_ptr enemy_sprite = bn::sprite_items::square.create_sprite(-30, 22);
     bn::rect enemy_bounding_box = create_bounding_box(enemy_sprite, ENEMY_SIZE);
