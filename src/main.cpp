2a191e846d49ab907f694416a359944310096d39
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-02-24 20:05:29 +0000

    updated the constructor and fixed the initialization in the enemy class

--- src/main.cpp
+++ src/main.cpp
@@ -136,14 +136,17 @@ class Player {
 
 class Enemy {
     public:
-        Enemy() :
-        sprite(bn::sprite_ptr::square.create_sprite(starting_x, starting_y)),
-        size(ENEMY_SIZE),
+        Enemy(int starting_x, int starting_y, bn::size enemy_size) :
+        sprite(bn::sprite_items::square.create_sprite(starting_x, starting_y)),
+        size(enemy_size),
+        bounding_box(create_bounding_box(sprite, size))
+    {}
 
-
-
-     
-}
+    // Create the sprite. This will be moved to a constructor
+        bn::sprite_ptr sprite = bn::sprite_items::square.create_sprite();
+        bn::size size; // The width and height of the sprite
+        bn::rect bounding_box; // The rectangle around the sprite for checking collision
+};
 
 int main() {
     bn::core::init();
@@ -154,16 +157,13 @@ int main() {
     // Create a player and initialize it
     // TODO: we will move the initialization logic to a constructor.
     Player player = Player(22, 44, 3.5, PLAYER_SIZE);
-    Enemy enemy = Enemy(-30,22);
-
-    // bn::sprite_ptr enemy_sprite = bn::sprite_items::square.create_sprite(-30, 22);
-    bn::rect enemy_bounding_box = create_bounding_box(enemy_sprite, ENEMY_SIZE);
+    Enemy enemy = Enemy(-30, 22, ENEMY_SIZE);
 
     while(true) {
         player.update();
 
         // Reset the current score and player position if the player collides with enemy
-        if(enemy_bounding_box.intersects(player.bounding_box)) {
+        if(enemy.bounding_box.intersects(player.bounding_box)) {
             scoreDisplay.resetScore();
             player.sprite.set_x(44);
             player.sprite.set_y(22);
